f76eacdd8d3f15b7e06500178c80c4929442b4c8
Jaehyun Lim <32118818+stiria06@users.noreply.github.com>
2023-04-05 20:02:26 +0900

    fix: merge error fixed at MainView.java

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -8,6 +8,7 @@ import dto.userdto.UserSession;
 
 public class MainView {
 	static Scanner sc = new Scanner(System.in);
+
 	/**
 	 * 메뉴 선택 (동작 선택)
 	 */
@@ -18,12 +19,13 @@ public class MainView {
 			System.out.print("[ " + selectionIndex++ + ". 게시판 조회   ");
 			System.out.print(selectionIndex++ + ". 게시글 작성   ");
 			System.out.print(selectionIndex++ + ". 게시글 검색   ");
-			System.out.print(selectionIndex++ + ". 내 게시글 조회   ");
+			System.out.print(selectionIndex++ + ". 내 게시글 조회/수정/삭제   ");
 			System.out.print(selectionIndex++ + ". 마이페이지   ");
 			System.out.print(selectionIndex++ + ". 로그아웃   ");
 			System.out.print(selectionIndex++ + ". 오늘의 식단   ");
-			if (UserSession.getInstance().isAdmin()) System.out.print(selectionIndex++ + ". 오늘의 식단 등록하기   ");
-			System.out.print(selectionIndex++ + ". 앱 종료 ]");
+			if (UserSession.getInstance().isAdmin())
+				System.out.print(selectionIndex++ + ". 오늘의 식단 등록하기   ");
+			System.out.print(selectionIndex + ". 앱 종료 ]");
 
 			System.out.println("\n--------------------------------------------");
 			System.out.println("원하는 작업을 숫자로 입력해주세요.");
@@ -49,12 +51,29 @@ public class MainView {
 					logOut();
 					return;
 				case 7:
+					checkTodayMeal();
+					break;
+				case 8:
+					if (UserSession.getInstance().isAdmin()) {
+						insertTodayMeal();
+						break;
+					}
 					System.out.println("게시판 사용을 종료합니다.");
 					System.exit(0);
 					break;
+				case 9:
+					if (UserSession.getInstance().isAdmin()) {
+						System.out.println("게시판 사용을 종료합니다.");
+						System.exit(0);
+						break;
+					} else
+						throw new NumberFormatException();
+				default:
+					throw new NumberFormatException();
 				}
+
 			} catch (NumberFormatException e) {
-				System.out.println("원하는 작업을 1 ~ 7 사이의 숫자로 입력해주세요.");
+				System.out.println("원하는 작업을 1 ~ " + selectionIndex + " 사이의 숫자로 입력해주세요.");
 			}
 		}
 
@@ -96,14 +115,12 @@ public class MainView {
 	 * 게시글 작성
 	 */
 	public static void insertBoard() {
-		System.out.println("제목은?");
-		String title = sc.nextLine();
-
 		System.out.println("게시판은?");
-		System.out.print("1. 공지");
-		System.out.print("2. 자유");
-		System.out.print("3. 질문");
+		System.out.print("1. 공지   ");
+		System.out.print("2. 자유   ");
+		System.out.print("3. 질문   ");
 		String subject = "";
+
 		try {
 			int menu = Integer.parseInt(sc.nextLine());
 			switch (menu) {
@@ -123,6 +140,9 @@ public class MainView {
 			System.out.println("원하는 게시판을 1 ~ 3 사이의 숫자로 입력해주세요.");
 		}
 
+		System.out.println("제목은?");
+		String title = sc.nextLine();
+
 		System.out.println("내용은?");
 		String content = sc.nextLine();
 
@@ -135,30 +155,55 @@ public class MainView {
 	 * 게시글 수정
 	 */
 	public static void updateBoard() {
-
+		
 	}
 
 	/**
 	 * 게시글 검색
 	 */
 	public static void searchBoard() {
+		System.out.println("검색 대상을 선택해주세요.");
+		System.out.print("1. 제목 + 내용    ");
+		System.out.println("2. 작성자");
+		String target = "";
+		try {
+			int menu = Integer.parseInt(sc.nextLine());
+			switch (menu) {
+			case 1:
+				target = "title&content";
+				break;
+			case 2:
+				target = "writer";
+				break;
+			default:
+				throw new NumberFormatException();
+			}
+		} catch (NumberFormatException e) {
+			System.out.println("원하는 게시판을 1 ~ 2 사이의 숫자로 입력해주세요.");
+		}
+
 		System.out.println("검색하려는 키워드를 입력해주세요.");
 		String keyword = sc.nextLine();
-		BoardController.boardSelectByKeyword(keyword);
+		BoardController.boardSelectByKeyword(target, keyword);
 	}
 
 	/**
 	 * 내 게시글 조회
 	 */
 	public static void checkMyBoard() {
+		// 1. 조회
+		// 2. 수정
+		// 3. 삭제
 
+		// 입력으로 boardNo, 조회 혹은 수정
+
+		BoardController.boardSelectByUserId(UserSession.getInstance().getUuid());
 	}
 
 	/**
 	 * 마이페이지
 	 */
 	public static void checkMyPage() {
-
 	}
 
 	/**
@@ -167,14 +212,13 @@ public class MainView {
 	public static void checkTodayMeal() {
 
 	}
-	
+
 	/**
 	 * 어드민 전용 식단 등록
 	 */
 	public static void insertTodayMeal() {
 
 	}
-
 
 	/**
 	 * 로그아웃
