7b32f0021d3b60e1d009ac6e735056491caf9885
seojisoo <amy0480@naver.com>
2023-04-05 16:03:23 +0900

    [ feat ] boardimpl 작성

--- src/service/BoardServiceImpl.java
+++ src/service/BoardServiceImpl.java
@@ -1,3 +1,6 @@
+/**
+ * 작성자 : 임재현, 서지수
+ */
 package service;
 
 import java.util.List;
@@ -6,42 +9,62 @@ import dto.boarddto.BoardDTO;
 import dto.replydto.ReplyDTO;
 import exception.DMLException;
 import exception.SearchWrongException;
+import dao.BoardDAO;
+import dao.BoardDAOImpl;
+import service.BoardService;
+import service.BoardServiceImpl;
 
 public class BoardServiceImpl implements BoardService {
-
+	private static BoardService instance = new BoardServiceImpl();
+	
+	private BoardDAO boardDAO = BoardDAOImpl.getInstance();
+	
+	private BoardServiceImpl() {}
+	public static BoardService getInstance() {
+		return instance;
+	}
+	
 	@Override
 	public List<BoardDTO> boardSelectAll() throws SearchWrongException {
-		// TODO Auto-generated method stub
-		return null;
+		List<BoardDTO> boardList = boardDAO.boardSelectAll();
+		if(boardList.size()==0)
+			throw new SearchWrongException("조회할 수 있는 게시글이 없습니다.");
+		
+		return boardList;
 	}
 
 	@Override
 	public List<BoardDTO> boardSelectBySubject(String subject) throws SearchWrongException {
-		// TODO Auto-generated method stub
-		return null;
+		List<BoardDTO> list = boardDAO.boardSelectBySubject(subject);
+		if(list.isEmpty()) throw new SearchWrongException(subject+"게시판의 게시글이 없습니다.");
+		return list;
 	}
 
 	@Override
 	public List<BoardDTO> boardSelectByKeyword(String keyWord) throws SearchWrongException {
-		// TODO Auto-generated method stub
-		return null;
+		List<BoardDTO> list = boardDAO.boardSelectBySubject(keyWord);
+		if(list.isEmpty()) throw new SearchWrongException(keyWord+"단어를 포함한 정보가 없습니다.");
+		return list;
 	}
 
 	@Override
 	public void boardInsert(BoardDTO boardDTO) throws DMLException {
-		// TODO Auto-generated method stub
+		int result = boardDAO.boardInsert(boardDTO);
+		if(result==0)throw new DMLException("게시글 등록에 실패했습니다.");
 		
 	}
 
 	@Override
 	public void boardUpdate(BoardDTO boardDTO) throws DMLException {
-		// TODO Auto-generated method stub
+		int result = boardDAO.boardUpdate(boardDTO);
+		if(result==0)throw new DMLException("게시글 수정에 실패했습니다.");
 		
 	}
 
 	@Override
 	public void boardDelete(int boardNo) throws DMLException {
-		// TODO Auto-generated method stub
+		int result = boardDAO.boardDelete(boardNo);
+		if(result==0)throw new DMLException("게시글 삭제에 실패했습니다.");
 		
 	}
 
