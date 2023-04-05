b77c27cddf7c118d334c3f6c47dae48fb9503430
kim <dnjsdn2468@naver.com>
2023-04-05 10:30:46 +0900

    first commit to KWW branch

--- src/common/DBProperties.java
+++ src/common/DBProperties.java
@@ -4,6 +4,6 @@ public interface DBProperties {
     String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
     String URL="jdbc:oracle:thin:@localhost:1521:XE";
 
-    String USER_ID = "c##mini";
-    String USER_PASS = "tiger";
+    String USER_ID = "c##team";
+    String USER_PASS = "team";
 }
