b28e1f0b4b5548a8265846cfcb452958285376b8
Jaehyun Lim <32118818+stiria06@users.noreply.github.com>
2023-04-05 13:04:06 +0900

    chore: edit typo at MainApp.java

--- src/view/MainApp.java
+++ src/view/MainApp.java
@@ -2,7 +2,7 @@ package view;
 
 public class MainApp {
 	public static void main(String[] args) {
-		System.out.println("************************** 프로그램 시작 **************************");
+		System.out.println("------ 프로그램 시작 ------");
 		while (true) {
 			LoginView.loginChoice();
 			MainView.menuChoice();
