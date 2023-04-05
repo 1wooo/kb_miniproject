98b70518e2c32878817d5e3319a2eb9d786e6845
Jaehyun Lim <32118818+stiria06@users.noreply.github.com>
2023-04-05 12:56:00 +0900

    feat: add default view at MainApp.js

--- src/view/MainApp.java
+++ src/view/MainApp.java
@@ -1,8 +1,11 @@
 package view;
 
 public class MainApp {
-    public static void main(String[] args) {
-        System.out.println("**************************프로그램 시작**************************");
-        MainView.menuChoice();
-    }
+	public static void main(String[] args) {
+		System.out.println("************************** 프로그램 시작 **************************");
+		while (true) {
+			LoginView.loginChoice();
+			MainView.menuChoice();
+		}
+	}
 }
